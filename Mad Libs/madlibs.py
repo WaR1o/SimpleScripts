75616062f8a9ca08c32d6a938af76b6878c9445d
WaR1o <shok163@gmail.com>
2016-08-12 11:31:34 +0400

    Update madlibs.py

--- Mad Libs/madlibs.py
+++ Mad Libs/madlibs.py
@@ -1,12 +1,9 @@
 #! python3
 
-# TODO: Reads text file
+# Reads text file
 text = open('D:\\code\\mycode\\MadLibs\\text.txt')
-# TODO: Finds the words ADJECTIVE, NOUN, ADVERB, VERB.
+# Finds the words ADJECTIVE, NOUN, ADVERB, VERB.
 words = ['ADJECTIVE', 'NOUN', 'ADVERB', 'VERB']
 for word in words:
     if word in text:
         print(word)
-# TODO: Ask for user input to replace that words.
-
-# TODO: Prints the result and save it to the text file.
