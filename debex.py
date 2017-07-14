1a287eefffbcf683979b700068ebe99e862bf55e
WaR1o <shok163@gmail.com>
2017-07-14 15:50:39 +0400

    Delete debex.py

--- debex.py
+++
@@ -1,8 +0,0 @@
-print('Enter the 1st number to add:')
-first = input()
-print('Enter the 2nd number to add:')
-second = input()
-print('Enter the 3rd number to add:')
-third = input()
-print('The sum is ' + str(int(first) + int(second) + int(third)))
-2
